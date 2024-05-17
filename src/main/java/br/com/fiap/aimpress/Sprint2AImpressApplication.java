9b84b4a9162f907e3586ac628c0b5cb3cc7a4844
EsparzaPhoenix <esparza_leonardo@hotmail.com>
2024-05-17 13:26:56 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fiap/aimpress/Sprint2AImpressApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aimpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint2AImpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint2AImpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/aimpress/Sprint2AImpressApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aimpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint2AImpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
